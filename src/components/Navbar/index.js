78536b95c21b2ebbcebe4c01661275875b90bf65
iggy <allyson.nostrandd@gmail.com>
2022-06-01 21:42:20 -0700

    nav links are functional!

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,15 +1,23 @@
 import React from 'react'
-// import '/.style.css'
-// import { Link } from 'react-router-dom'
+import './style.css'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav>
         <ul>
+          <Link to="/about">
             <li>About Me</li>
+          </Link>
+          <Link to="/contact">
             <li>Contact</li>
+          </Link>
+          <Link to="/portfolio"> 
             <li>Portfolio</li>
+          </Link> 
+          <Link to="/resume">
             <li>Resume</li>
+          </Link>
         </ul>
     </nav>
   )
