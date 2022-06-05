6e42cd41d65d1db9739171035bf3b0dd350e7dba
iggy <allyson.nostrandd@gmail.com>
2022-06-05 13:34:43 -0700

    created simple resume page with proficiencies and a link to resume pdf

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,6 +2,20 @@ import React from 'react'
 
 export default function Resume() {
   return (
-    <div>Resume</div>
+    <div>
+    <div className='resumeskills'>
+      <h2>Proficiencies</h2>
+        <ul className='skilllist'>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>Mysql</li>
+          <li>Mongoose</li>
+          <li>Handlebars</li>
+          <li>React</li>
+        </ul>
+    </div>
+    <h3><a href='https://drive.google.com/file/d/1RL_iHVA8p0kWARmMAvgv_GySDpAa_XP9/view?usp=sharing' target="_blank" rel='noreferrer noopener'>Download my resume here</a></h3>
+    </div>
   )
 }
