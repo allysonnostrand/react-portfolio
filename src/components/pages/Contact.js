b4c2fc4cd374e2d18e4490a3fb5c3f24e1288ffb
iggy <allyson.nostrandd@gmail.com>
2022-06-27 14:37:22 -0700

    new resume link and remodeled contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,27 +2,20 @@ import React from 'react'
 
 export default function Contact () {
   return (
-  
-  <div className='contactbox'>
-    <div className='boxitems'>
-      <h3 className='contacthead'>Please feel free to reach out!</h3>
-        <form>
-          <div className='col'>
-            <label>Name: </label>
-            <input type='input' required></input>
-          </div>
-          <div className='col'>
-            <label>Email: </label>
-            <input type='email' required></input>
-          </div>
-          <div className='col message'>
-            <label>Message: </label>
-            <textarea type='input' required></textarea>
-          </div>
-          <div>
-            <button>Submit</button>
-          </div>
-        </form>
+
+    <div className='contactbox'>
+      <div className='boxitems'>
+        <h3 className='contacthead'>Please feel free to reach out!</h3>
+        <p>Email: allyson.nostrandd@gmail.com</p>
+        <div className='extra'>
+          <h3 className='extrahead'>You can also find me on</h3>
+          Github: <a className='contactlinks' href='https://github.com/allysonnostrand' target="_blank" rel='noreferrer noopener'>https://github.com/allysonnostrand
+          </a>
+          <br></br>
+          LinkedIn: <a className='contactlinks' href='https://linkedin.com/in/allyson-nostrand-912910154' target="_blank" rel='noreferrer noopener'>https://linkedin.com/in/allyson-nostrand-912910154
+          </a>
+        </div>
+        <h4>Thank you for visiting!</h4>
       </div>
     </div>
   )
