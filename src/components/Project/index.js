c8c5e98ace0d387a69c89c0c4f773008a5aa0a36
iggy <allyson.nostrandd@gmail.com>
2022-08-02 19:28:18 -0700

    made some small changes to project card component

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -7,11 +7,13 @@ const Project = (props) => {
   return (
       <div className='card' style={{backgroundImage: `url(${process.env.PUBLIC_URL + props.image})`,backgroundSize: 'cover'}}>
         <div className='text'>
-          <h2>{props.title}</h2>
-          <a href={props.deployed} target="_blank" rel='noreferrer noopener'>Deployed
-          </a>
-          <a href={props.repo} target="_blank" rel='noreferrer noopener'>Repo
-          </a>
+          <h3>{props.title}</h3>
+          <div className='project-links'>
+            <a href={props.deployed} target="_blank" rel='noreferrer noopener'>Deployed
+            </a>
+            <a href={props.repo} target="_blank" rel='noreferrer noopener'>Repo
+            </a>
+          </div>
         </div>
       </div>
       
