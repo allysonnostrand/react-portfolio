2c19cdbfebc98c043ddd64f5fcf9839cdb9f1d7c
ArshErgon <arshergon@gmail.com>
2022-08-07 17:58:52 +0530

    made some changes

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,23 +1,35 @@
-import React from 'react'
+import React from "react";
+import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
+import { IconContext } from "react-icons";
 
 const ContactForm = () => {
   return (
-    <div className='contactbox' id='contact'>
-          <div className='boxitems'>
-            <h3 className='contacthead'>Please feel free to reach out!</h3>
-            <p>Email: allyson.nostrandd@gmail.com</p>
-            <div className='extra'>
-              <h3 className='extrahead'>You can also find me on</h3>
-              Github: <a className='contactlinks' href='https://github.com/allysonnostrand' target="_blank" rel='noreferrer noopener'>https://github.com/allysonnostrand
-              </a>
-              <br></br>
-              LinkedIn: <a className='contactlinks' href='https://linkedin.com/in/allyson-nostrand-912910154' target="_blank" rel='noreferrer noopener'>https://linkedin.com/in/allyson-nostrand-912910154
-              </a>
-            </div>
-            <h4>Thank you for visiting!</h4>
-          </div>
-        </div>
-  )
-}
+    <div className="contactbox" id="contact">
+      <div className="boxitems">
+        <h3 className="contacthead">Please feel free to reach out!</h3>
+        <IconContext.Provider value={{ color: "white" }}>
+          <a
+            className="footernav"
+            style={{ padding: "0 5pc 0 0" }}
+            href="https://github.com/allysonnostrand"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <FaGithubSquare className="footer-icon" />
+          </a>
+          <a
+            className="footernav"
+            href="https://linkedin.com/in/allyson-nostrand-912910154"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <FaLinkedin className="footer-icon" />
+          </a>
+        </IconContext.Provider>
+        <h4>Thank you for visiting!</h4>
+      </div>
+    </div>
+  );
+};
 
-export default ContactForm
+export default ContactForm;
