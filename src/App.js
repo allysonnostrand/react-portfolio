cd8c6bcb0d8ce649e2d620ee9849246944357436
iggy <allyson.nostrandd@gmail.com>
2023-01-26 13:26:18 -0800

    merge conflicts

--- src/App.js
+++ src/App.js
@@ -50,12 +50,8 @@ function App(props) {
         </div>
         
         <footer>
-<<<<<<< HEAD
           
           <IconContext.Provider value={{color: "rgb(144, 238, 144)"}}>
-=======
-          <IconContext.Provider value={{color: "white"}}>
->>>>>>> parent of ee51a15 (added new styling)
             <a className='footernav' href='https://github.com/allysonnostrand' target="_blank" rel='noreferrer noopener'><FaGithubSquare className='footer-icon' />
             </a>
             <ContactForm />
